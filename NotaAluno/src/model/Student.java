6e9b102cb4ce07a7c80fe7f30e039d7fdebe98e9
Erick Rennan Carvalho de Oliveira dos Santos Coelho <ercosc@gmail.com>
2017-08-01 03:20:42 -0300

    Exercício LTP1

---
+++ NotaAluno/src/model/Student.java
@@ -0,0 +1,56 @@
+package model;
+
+import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+
+public class Student 
+{
+	private String nome;
+	private ArrayList<String> notas;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public ArrayList<String> getNotas() {
+		return notas;
+	}
+	public void setNotas(ArrayList<String> notas) {
+		this.notas = notas;
+	}
+	
+	public int notasSs(ArrayList<String> Student) 
+	{
+		
+		return notas.size();
+	}
+	public int arrayNotas(ArrayList<String> notas)
+	{
+		String notasSs;
+		int totNotas=0;
+		int i;
+		return 0;
+	//	while(totNotas<((Student) notas).getsize())
+		//
+			//if(notas.get(notasSs) = "ss")
+		//	{
+			//	;
+				//totNotas++;
+				//i++;
+		//	}
+		//	else;
+		//totNotas++;
+		//return 0;
+	}
+	//private int getsize() {
+		// TODO Auto-generated method stub
+		//return 0;
+	//}
+	
+	
+
+}
